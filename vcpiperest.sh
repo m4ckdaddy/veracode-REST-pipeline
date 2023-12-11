f12087ac9a88c8c545bd5a9c7e4138a43d7a6957
Jin Mok <102392728+mokkang@users.noreply.github.com>
2023-12-11 15:36:41 -0800

    Update vcpiperest.sh

--- vcpiperest.sh
+++ vcpiperest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # CLEAR INPUT.JSON for SANITY CHECK
 touch input.json
-echo ' ' > input.json
+#echo ' ' > input.json
 http --auth-type=veracode_hmac POST "https://api.veracode.com/pipeline_scan/v1/scans" < input.json > firstout.json
 python3 pipeline.py
 echo "Fetching Scan Results"
